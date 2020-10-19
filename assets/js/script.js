6226e1932f096b162dcb9c7c4e31d377505d8f59
ArtV97 <arthurvianna@id.uff.br>
2020-10-19 11:23:22 -0300

    form ok

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,19 +14,21 @@ $(function(){
     });
 
     $('#botao_cadastrar').click(function(){
+        let form;
         let nome = valida_text_box('#nome_cadastro');
         let sexo = valida_sexo_radio_button();
         let email = valida_text_box('#email_cadastro');
         let senha = valida_text_box('#senha_cadastro');
-        let senha_confirmar = valida_text_box('#confirmar_senha_cadastro');
+        let senha_confirmar = confirma_senha('#senha_cadastro', '#confirmar_senha_cadastro');
         let cep = valida_text_box('#cep_cadastro');
         let cidade = valida_text_box('#cidade_cadastro');
         let estado = valida_combo_box('#estado_cadastro');
         let endereco = valida_text_box('#endereco_cadastro');
+        let interesses = valida_check_box('.interesses');
         // let complemento = valida_text_box('#complemento_cadastro'); //nao é obrigatório
         
         //fazer um AND com todos os campos
-        if(nome && sexo && email && senha && cep && estado && cidade && endereco){
+        if(nome && sexo && email && senha && senha_confirmar && cep && estado && cidade && endereco && interesses){
             alert("Criando usuário no BD!");
         }
         else{
@@ -46,6 +48,21 @@ function valida_text_box(param){
     else{
         id_text_box.addClass("is-valid");
         id_text_box.removeClass("is-invalid");
+        return true;
+    }
+}
+
+function confirma_senha(id_senha, id_confirma_senha){
+    let senha = $(id_senha);
+    let confirma_senha = $(id_confirma_senha);
+    if(senha.val() != confirma_senha.val()){
+        confirma_senha.addClass("is-invalid");
+        confirma_senha.removeClass("is-valid");
+        return false;
+    }
+    else if(confirma_senha.val() != ''){
+        confirma_senha.removeClass("is-invalid");
+        confirma_senha.addClass("is-valid");
         return true;
     }
 }
@@ -83,6 +100,24 @@ function valida_combo_box(param){
     else{
         id_combo_box.addClass("is-valid");
         id_combo_box.removeClass("is-invalid");
+        return true;
+    }
+}
+
+function valida_check_box(param){
+    let id_check_box = $(`input${param}`);
+    if($(`input${param}:checked`).length === 0){
+        $.each(id_check_box, function(index, value){
+            $(`#${value.id}`).addClass("is-invalid");
+            $(`#${value.id}`).removeClass("is-valid");
+        });
+        return false;
+    }
+    else{
+        $.each(id_check_box, function(index, value){
+            $(`#${value.id}`).removeClass("is-invalid");
+            $(`#${value.id}`).addClass("is-valid");
+        });
         return true;
     }
 }
